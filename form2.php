65f648f2f8662723f4841dd418c04f2149757b90
Cynthia Le <Cynthia Le>
2016-10-24 16:12:59 -0700

    Initial commit Judge eval form

---
+++ form2.php
@@ -0,0 +1,36 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Judge Evaluation Form</title>
+  <meta name="description" content="Judge Eval Form page 1">
+  <meta name="author" content="Group 1">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+  <h1>Senior Design Conference Judge Evaluation Form</h1>
+  <hr/>
+
+  Judge ID: <br>
+  Judge Name: <br>
+  <form action="form3.html">
+    Project Title: <input list="projects" name="project"><br>
+    <datalist id="projects">
+      <option value="val1">
+      <option value="val2">
+      <option value="val3">
+    </datalist>
+    <input type="submit" value="Submit">
+  </form>
+  <script src="js/scripts.js"></script>
+
+</body>
+</html>
