f012c7f1c20012a28afece51c91e41c6f455566c
Karsten Andersen <Me@Karstens-MacBook-Pro.local>
2016-10-30 15:20:11 -0700

    Added capabilities to getRepDet.php

--- getDetRep.php
+++ getDetRep.php
@@ -18,7 +18,7 @@
       return;
       }
 
-    $sql = "SELECT PID, pTitle, dept FROM Presentations";
+    $sql = "SELECT Presentations.PID, pTitle, dept, advisor, student FROM Presentations INNER JOIN Students ON Presentations.PID=Students.PID";
 
     // $sql = "SELECT * FROM Comments ORDER BY date_added DESC LIMIT 10";
 
