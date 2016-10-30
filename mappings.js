3ec05a0f41ede7f8c1d5ee874a08e6a22f2f7ba0
Cynthia Le <Cynthia Le>
2016-10-30 12:04:35 -0700

    add connection between form pages

---
+++ mappings.js
@@ -0,0 +1,23 @@
+formMap={
+DP1: "Technical Accuracy",
+DP2: "Creativity and Innovation",
+DP3: "Supporting Analytical Work",
+DP4: "Methodical Design Process Demonstrated",
+DP5: "Addresses Project Complexity Appropriately",
+DP6: "Expectation of Completion",
+DP7: "Design and Analysis of Tests",
+DP8: "Quality of Response during Q&A",
+P1: "Organization",
+P2: "Use of Alotted Time",
+P3: "Visual Aids",
+P4: "Confidence and Poise",
+C1: "Economic",
+C2: "Sustainability",
+C3: "Ethical",
+C4: "Social",
+C5: "Environmental",
+C6: "Manufacturability",
+C7: "Health and Safety",
+C8: "Political"
+};
+
